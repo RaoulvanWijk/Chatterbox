fb630d58080addf770565fc5c04e653a8f59e6bd
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-09-29 15:34:55 +0200

    added client side validation for the register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,45 +1,68 @@
-"use client"
+"use client";
 
-import React, { useRef } from 'react'
-import Link from 'next/link'
+import React, { useRef } from "react";
+import Link from "next/link";
+import { registerSchema, TRegisterSchema } from "@/lib/types/zodSchemes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function register() {
-  const username = useRef<HTMLInputElement>(null)
-  const email = useRef<HTMLInputElement>(null)
-  const password = useRef<HTMLInputElement>(null)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    setError,
+  } = useForm<TRegisterSchema>({
+    resolver: zodResolver(registerSchema),
+  });
 
-  const submitHandler = (e: React.FormEvent) => {
-    e.preventDefault()
-    fetch('/api/auth/register', {
-      method: 'POST',
+  const onSubmit = async (data: TRegisterSchema) => {
+    console.log(data);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        username: username.current.value,
-        email: email.current.value,
-        password: password.current.value
-      })
-    })
-  }
+      body: JSON.stringify(data),
+    });
+
+    const resData = await res.json();
+
+    if (!res.ok) {
+      // response status is not 2xx
+      alert(
+        "Something went wrong while submitting the form. Please try again later."
+      );
+      return;
+    }
+  };
+
   return (
-    <form className='auth-form' onSubmit={submitHandler}>
-      <h1 className='title'>Welcome new user!</h1>
-      <p className='text'>We are happy for you to join us!</p>
-      <label htmlFor="username">
-        Username
-      </label>
-      <input type="text" name="username" id="username" ref={username} />
-      <label htmlFor="email">
-        Email
-      </label>
-      <input type="email" name="email" id="email" ref={email} />
-      <label htmlFor="password">
-        Password
-      </label>
-      <input type="password" name="password" id="password" ref={password} />
-      <button className='main-btn' type="submit">Register</button>
-      <Link className='disclaimer' href='/auth/login'>Already have an account? <span>Login</span></Link>
+    <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="title">Welcome new user!</h1>
+      <p className="text">We are happy for you to join us!</p>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" {...register("username")} />
+      {errors.username && (
+        <p className="text-red-500">{`${errors.username.message}`}</p>
+      )}
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" {...register("email")} />
+      {errors.email && (
+        <p className="text-red-500">{`${errors.email.message}`}</p>
+      )}
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" {...register("password")} />
+      {errors.password && (
+        <p className="text-red-500">{`${errors.password.message}`}</p>
+      )}
+      <button className="main-btn" type="submit">
+        Register
+      </button>
+      <Link className="disclaimer" href="/auth/login">
+        Already have an account? <span>Login</span>
+      </Link>
     </form>
-  )
+  );
 }
