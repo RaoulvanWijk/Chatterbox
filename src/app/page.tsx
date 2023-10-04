a746713597ac1741c5012107b6b05e0bada38135
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-04 14:31:47 +0200

    WIP with testing cookie fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import Image from 'next/image'
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { useEffect } from 'react';
 
 
 export default function Home() {
+  const { data: session, status, update } = useSession()
+  useEffect(() => {
+  }, [session])
+  
   return (
     <>
+      {/* check if session is loading */}
+      {/* check if session is set */}
+      <h1>Home</h1>
+      
       <button onClick={() => signIn()}>Login</button>
     </>
   )
