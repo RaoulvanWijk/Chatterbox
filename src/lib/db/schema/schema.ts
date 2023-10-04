611790774c52937f8c688cf38444757a4ec492df
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-04 14:31:17 +0200

    added some more fields to db schema

--- src/lib/db/schema/schema.ts
+++ src/lib/db/schema/schema.ts
@@ -1,58 +1,64 @@
-import { mysqlTable, mysqlSchema, AnyMySqlColumn, index, varchar, int, unique, timestamp, bigint } from "drizzle-orm/mysql-core"
-import { sql } from "drizzle-orm"
-
-
-export const account = mysqlTable("account", {
-	userId: varchar("userId", { length: 255 }).notNull(),
-	type: varchar("type", { length: 255 }).notNull(),
-	provider: varchar("provider", { length: 255 }).notNull(),
-	providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-	refreshToken: varchar("refresh_token", { length: 255 }),
-	accessToken: varchar("access_token", { length: 255 }),
-	expiresAt: int("expires_at"),
-	tokenType: varchar("token_type", { length: 255 }),
-	scope: varchar("scope", { length: 255 }),
-	idToken: varchar("id_token", { length: 255 }),
-	sessionState: varchar("session_state", { length: 255 }),
-},
-(table) => {
-	return {
-		userIdUserIdFk: index("account_userId_user_id_fk").on(table.userId),
-	}
-});
-
-export const computers = mysqlTable("computers", {
-	id: bigint("id", { mode: "number" }).autoincrement().notNull(),
-	brand: varchar("brand", { length: 256 }).notNull(),
-	cores: int("cores").notNull(),
-},
-(table) => {
-	return {
-		id: unique("id").on(table.id),
-	}
-});
-
-export const session = mysqlTable("session", {
-	sessionToken: varchar("sessionToken", { length: 255 }).notNull(),
-	userId: varchar("userId", { length: 255 }).notNull(),
-	expires: timestamp("expires", { mode: 'string' }).notNull(),
-},
-(table) => {
-	return {
-		userIdUserIdFk: index("session_userId_user_id_fk").on(table.userId),
-	}
-});
-
-export const user = mysqlTable("user", {
-	id: varchar("id", { length: 255 }).notNull(),
-	name: varchar("name", { length: 255 }),
-	emailVerified: timestamp("emailVerified", { fsp: 3, mode: 'string' }).default('CURRENT_TIMESTAMP(3)').notNull(),
+import {
+	int,
+	timestamp,
+	mysqlTable,
+	primaryKey,
+	varchar
+  } from "drizzle-orm/mysql-core"
+  import type { AdapterAccount } from "@auth/core/adapters"
+  
+  export const users = mysqlTable("user", {
+	id: varchar("id", { length: 255 }).notNull().primaryKey(),
+	username: varchar("username", { length: 255 }),
+	password: varchar("password", { length: 255 }),
 	email: varchar("email", { length: 255 }).notNull(),
-	image: varchar("image", { length: 255 }),
-});
-
-export const verificationtoken = mysqlTable("verificationtoken", {
-	identifier: varchar("identifier", { length: 255 }).notNull(),
-	token: varchar("token", { length: 255 }).notNull(),
-	expires: timestamp("expires", { mode: 'string' }).notNull(),
-});
+	emailVerified: timestamp("emailVerified", {
+	  mode: "date",
+	  fsp: 3
+	}).defaultNow(),
+	image: varchar("image", { length: 255 })
+  })
+  
+  export const accounts = mysqlTable(
+	"account",
+	{
+	  userId: varchar("userId", { length: 255 })
+		.notNull()
+		.references(() => users.id, { onDelete: "cascade" }),
+	  type: varchar("type", { length: 255 })
+		.$type<AdapterAccount["type"]>()
+		.notNull(),
+	  provider: varchar("provider", { length: 255 }).notNull(),
+	  providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+	  refresh_token: varchar("refresh_token", { length: 255 }),
+	  access_token: varchar("access_token", { length: 255 }),
+	  expires_at: int("expires_at"),
+	  token_type: varchar("token_type", { length: 255 }),
+	  scope: varchar("scope", { length: 255 }),
+	  id_token: varchar("id_token", { length: 255 }),
+	  session_state: varchar("session_state", { length: 255 })
+	},
+	(account) => ({
+	  compoundKey: primaryKey(account.provider, account.providerAccountId)
+	})
+  )
+  
+  export const sessions = mysqlTable("session", {
+	sessionToken: varchar("sessionToken", { length: 255 }).notNull().primaryKey(),
+	userId: varchar("userId", { length: 255 })
+	  .notNull()
+	  .references(() => users.id, { onDelete: "cascade" }),
+	expires: timestamp("expires", { mode: "date" }).notNull()
+  })
+  
+  export const verificationTokens = mysqlTable(
+	"verificationToken",
+	{
+	  identifier: varchar("identifier", { length: 255 }).notNull(),
+	  token: varchar("token", { length: 255 }).notNull(),
+	  expires: timestamp("expires", { mode: "date" }).notNull()
+	},
+	(vt) => ({
+	  compoundKey: primaryKey(vt.identifier, vt.token)
+	})
+  )
