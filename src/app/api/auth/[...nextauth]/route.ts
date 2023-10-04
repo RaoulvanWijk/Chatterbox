244bb62acf44fd022095d4daba61dfef5530dc00
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-04 14:30:04 +0200

    added login functionality but still contains a bug with the cookie

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,66 @@
 import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
+import GoogleProvider from "next-auth/providers/google"
+import { DrizzleAdapter } from "@/lib/db/dbAdaper";
+import { db } from "@/lib/db/index"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { loginSchema, TLoginSchema } from "@/lib/types/zodSchemes";
+
+import bcrypt from 'bcryptjs';
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: DrizzleAdapter(db),
+  session: {
+    strategy: "jwt",
+    maxAge: 3000,
+    user: {
+      id: 'id',
+      username: 'username',
+      email: 'email',
+      password: 'password',
+      image: 'image',
+    },
+ }, 
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        
+        const res = loginSchema.safeParse(credentials);
+        if(!res.success) {
+          return null;
+        }
+        const da : any = DrizzleAdapter(db);
+        const user = await da.getUserByEmail(res.data.email);
+        if(!user) {
+          return null;
+        }
+        const passwordMatch = await bcrypt.compareSync(res.data.password, user.password);
+        if(!passwordMatch) {
+          return null;
+        }
+        return user;
+      }
     }),
-    // ...add more providers here
   ],
   pages: {
-    signIn: '/auth/login',
-    signOut: '/auth/signout',
-  }
+    signIn: "/auth/login",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+  },
+  callbacks: {
+    session({ session, token, user }) {
+      console.log("session", session, token, user);
+      
+      return session // The return type will match the one returned in `useSession()`
+    },
+  },
 }
-
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST };
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,19 +1,55 @@
+"use client";
+
+
 import React from 'react'
 import Link from 'next/link'
+import { signIn, getSession, getCsrfToken } from "next-auth/react";
+import { loginSchema, TLoginSchema } from "@/lib/types/zodSchemes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    setError,
+  } = useForm<TLoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async (data: TLoginSchema) => {
+    const res = await signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: true,
+      callbackUrl: "/",
+    });
+    // if(res.error) {
+    //   alert('You have entered an invalid email or password. Please try again.');
+    // }
+  };
+
+
   return (
-    <form className='auth-form' action="">
+    <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
       <h1 className='title'>Welcome back!</h1>
       <p className='text'>We are happy to see you back!</p>
       <label htmlFor="email">
         Email
       </label>
-      <input type="text" name="email" id="email" />
+      <input type="text" id="email" {...register("email")} />
+      {errors.email && (
+        <p className="text-red-500">{`${errors.email.message}`}</p>
+      )}
       <label htmlFor="password">
         Password
       </label>
-      <input type="password" name="password" id="password" />
+      <input type="password" id="password" {...register("password")} />
+      {errors.password && (
+        <p className="text-red-500">{`${errors.password.message}`}</p>
+      )}
       <button className='main-btn' type="submit">Login</button>
       <Link className='disclaimer' href='/auth/register'>Don't have an account yet? <span>Register</span></Link>
     </form>
