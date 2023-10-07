43345bc56f0567ae04c486699581f0cd823b5b8c
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-07 17:06:03 +0200

    added logout button to channel page

--- src/app/app/channels/[channel]/page.tsx
+++ src/app/app/channels/[channel]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import LogoutButton from '@/components/auth/LogoutButton'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <LogoutButton />
+      <p>
+        This is the page for a channel.
+      </p>
+      </div>
   )
 }
