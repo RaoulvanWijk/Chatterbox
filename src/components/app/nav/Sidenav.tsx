e1a32e0b4706acd19bd393826aebb4d75e5e4255
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-11 14:08:50 +0200

    fixed scrolling in sidenav

--- src/components/app/nav/Sidenav.tsx
+++ src/components/app/nav/Sidenav.tsx
@@ -1,28 +1,32 @@
-
-import React from 'react'
-import NavItem from './NavItem'
-import "@resources/styles/components/nav.scss"
+import React from "react";
+import NavItem from "./NavItem";
+import "@resources/styles/components/nav.scss";
 
 export default function Sidenav() {
   return (
-    <nav className='app-layout-content sidenav'>
+    <nav className="app-layout-content sidenav">
       <ul>
-        <NavItem href={'/app/channels/@me'} name={'CB'} active={true} />
-        
-        <div className='nav-seperator' />
+        <NavItem href={"/app/channels/@me"} name={"CB"} active={true} />
+
+        <div className="nav-seperator" />
+        <NavItem href={"/app/channels/1/1"} name={"SC"} />
+        <NavItem href={"/app/channels/1/1"} name={"SC"} />
+        <NavItem href={"/app/channels/1/1"} name={"SC"} />
+        <NavItem href={"/app/channels/1/1"} name={"SC"} />
+        <NavItem href={"/app/channels/1/1"} name={"SC"} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
-        {/* <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
         <NavItem href={'/app/channels/1/1'} name={'SC'} />
-        <NavItem href={'/app/channels/1/1'} name={'SC'} /> */}
+        <NavItem href={'/app/channels/1/1'} name={'SC'} />
+        <NavItem href={'/app/channels/1/1'} name={'SC'} />
       </ul>
     </nav>
-  )
+  );
 }
