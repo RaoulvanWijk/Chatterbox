d3b03b73b69d56c18c5b5f0e90f364792f37dcbb
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-12 15:31:54 +0200

    fixed overflow in chat

--- src/components/app/chat/Chat.tsx
+++ src/components/app/chat/Chat.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 import "@resources/styles/components/chat.scss"
+import Message from './Message'
 
 export default function Chat() {
   return (
     <div className='chat-container'>
-      <div className="active-chat-user">
+      <div className="active-chat-user app-layout-content">
         <p>Username#1234</p>
       </div>
-      <div className="app-layout-content chat-content"></div>
+      <div className="app-layout-content chat-content">
+        <ul>
+          <Message />
+          <Message />
+        </ul>
+      </div>
       <div className="app-layout-content chat-input">
-        <input name="message" id="" placeholder='Message @Username#1234' />
+        <input name="message" id="" placeholder='Message Username#1234' />
       </div>
     </div>
   )
