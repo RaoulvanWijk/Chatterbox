329997d90ad83997e278e605d3938242785c11a7
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-23 10:15:57 +0200

    styled a chat message

--- src/components/app/chat/Chat.tsx
+++ src/components/app/chat/Chat.tsx
@@ -4,6 +4,7 @@ import "@resources/styles/components/chat.scss"
 import Message from './Message'
 
 export default function Chat() {
+  const testMsg = {username: 'test', message: 'test', date: '2023-10-23T08:02:44.529Z' }
   return (
     <div className='chat-container'>
       <div className="active-chat-user app-layout-content">
@@ -11,8 +12,8 @@ export default function Chat() {
       </div>
       <div className="app-layout-content chat-content">
         <ul>
-          <Message />
-          <Message />
+          <Message message={testMsg} />
+          <Message message={testMsg} />
         </ul>
       </div>
       <div className="app-layout-content chat-input">
--- src/components/app/chat/Message.tsx
+++ src/components/app/chat/Message.tsx
@@ -1,6 +1,42 @@
+
 import React from 'react'
 
-export default function Message() {
+export type MessageProps = {
+    message: {
+        username: string
+        message: string
+        date: string
+    }
+}
+
+export default function Message(messageProps: MessageProps) {
+    const message = messageProps.message
+
+    let covertedDate = new Date(message.date)
+    let newDate
+    if(covertedDate.getDate() == (new Date()).getDate()) {
+        newDate = covertedDate.toLocaleString(
+            'en-US',
+            {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true
+            }
+        ).replace(',', ' ')
+        newDate = 'Today at ' + newDate
+    } else {
+        newDate = covertedDate.toLocaleString(
+            'en-US',
+            {
+                month: '2-digit',
+                day: '2-digit',
+                year: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true
+            }
+        ).replace(',', ' ')
+    }
     return (
         <li className='chat-message'>
             <div className='message-avatar'>
@@ -8,8 +44,8 @@ export default function Message() {
             </div>
 
             <div className='message-content'>
-                <p className='message-credentials'>ThatDutchGuy <span className='message-date'>Today at 10:30 AM</span></p>
-                <p className="memssage">Lorem ipsum dolor sit amet.</p>
+                <p className='message-credentials'>{message.username} <span className='message-date'>{newDate}</span></p>
+                <p className="memssage">{message.message}</p>
             </div>
         </li>
     )
