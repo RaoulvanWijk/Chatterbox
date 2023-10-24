2b07674159e1c08b69f0336a2e0d1a119fc55a2f
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-24 11:43:06 +0200

    setup sockets

--- src/app/app/channels/[channel]/[chat]/page.tsx
+++ src/app/app/channels/[channel]/[chat]/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+import { useEffect } from 'react'
+import { io, Socket } from 'socket.io-client'
+
 import LogoutButton from '@/components/auth/LogoutButton'
 import "@resources/styles/pages/app.scss"
 import Sidenav from '@/components/app/nav/Sidenav'
@@ -6,7 +9,38 @@ import FriendslistNav from '@/components/app/nav/FriendslistNav'
 import Chat from '@/components/app/chat/Chat'
 import ChatUsers from '@/components/app/chat/ChatUsers'
 
+
 export default function page() {
+  let socket: any
+  async function socketInitializer() {
+    await fetch("/api/socket");
+    console.log("socket initialized");
+
+    socket = io('http://localhost:3000/', {
+      path: "/api/socket.io",
+    });
+    console.log(socket);
+
+    socket.on("receive-message", (data: any) => {
+      // setAllMessages((pre) => [...pre, data]);
+    });
+    // return socket
+  }
+
+  useEffect(() => {
+    (async () => {
+      await socketInitializer();
+
+      return () => {
+        console.log(socket);
+
+        socket.disconnect();
+      };
+    })()
+  }, []);
+
+
+
   return (
     <main className="appBackground">
       <Sidenav />
---
+++ src/pages/api/socket.ts
@@ -0,0 +1,30 @@
+import { Server } from "socket.io";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function SocketHandler(req: NextApiRequest,
+    res: NextApiResponse) {
+        if (!res.socket) {
+            res.end();
+            return;
+        }
+        let socket = res.socket as any;
+
+        const io = new Server(socket.server, {
+            path: "/api/socket.io",
+        });
+
+        if (!socket.server.io) {
+            socket.server.io = io;
+        }
+        res.end();
+
+    io.on("connection", (socket) => {
+        console.log("Socket connected");
+        // socket.on("send-message", (obj) => {
+        //     io.emit("receive-message", obj);
+        // });
+    });
+
+    console.log("Setting up socket");
+    res.end();
+}
