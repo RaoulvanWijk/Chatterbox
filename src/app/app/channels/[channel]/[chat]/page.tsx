b55692eef052cc81117c3e461f8aeb0ed2489e12
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-10-25 16:41:58 +0200

    moved socket to chat

--- src/app/app/channels/[channel]/[chat]/page.tsx
+++ src/app/app/channels/[channel]/[chat]/page.tsx
@@ -1,6 +1,3 @@
-"use client"
-import { useEffect } from 'react'
-import { io, Socket } from 'socket.io-client'
 
 import LogoutButton from '@/components/auth/LogoutButton'
 import "@resources/styles/pages/app.scss"
@@ -11,36 +8,6 @@ import ChatUsers from '@/components/app/chat/ChatUsers'
 
 
 export default function page() {
-  let socket: any
-  async function socketInitializer() {
-    await fetch("/api/socket");
-    console.log("socket initialized");
-
-    socket = io('http://localhost:3000/', {
-      path: "/api/socket.io",
-    });
-    console.log(socket);
-
-    socket.on("receive-message", (data: any) => {
-      // setAllMessages((pre) => [...pre, data]);
-    });
-    // return socket
-  }
-
-  useEffect(() => {
-    (async () => {
-      await socketInitializer();
-
-      return () => {
-        console.log(socket);
-
-        socket.disconnect();
-      };
-    })()
-  }, []);
-
-
-
   return (
     <main className="appBackground">
       <Sidenav />
--- src/components/app/chat/Chat.tsx
+++ src/components/app/chat/Chat.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
 import "@resources/styles/components/chat.scss"
 import Message from './Message'
+import { useEffect } from 'react'
+import { io, Socket } from 'socket.io-client'
 
 export default function Chat() {
+
+  let socket: any
+  async function socketInitializer() {
+    await fetch("/api/socket");
+    console.log("socket initialized");
+
+    socket = io('http://localhost:3000/', {
+      path: "/api/socket.io",
+    });
+    console.log(socket);
+
+    socket.on("receive-message", (data: any) => {
+      // setAllMessages((pre) => [...pre, data]);
+    });
+    // return socket
+  }
+
+  useEffect(() => {
+    (async () => {
+      await socketInitializer();
+
+      return () => {
+        console.log(socket);
+
+        socket.disconnect();
+      };
+    })()
+  }, []);
+
   const testMsg = {username: 'test', message: 'test', date: '2023-10-23T08:02:44.529Z' }
   return (
     <div className='chat-container'>
